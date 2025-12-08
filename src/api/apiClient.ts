d28d0118e5ca7466c42dba4530fe47fbb38f7b93
Brian Dorantes <briandorantes@hotmail.com>
2025-12-08 13:54:48 -0600

    prueba de env 4

--- src/api/apiClient.ts
+++ src/api/apiClient.ts
@@ -1,7 +1,7 @@
 import { store } from "../store/store";
 import {setSession} from "../store/slices/authSlice";
+import { API_URL } from "../utils/env";
 
-const API_URL = import.meta.env.VITE_API_URL;
 
 export async function apiFetch<T>(
   endpoint: string,
---
+++ src/utils/env.ts
@@ -0,0 +1,3 @@
+// utils/env.ts
+export const API_URL = window.__ENV__.VITE_API_URL;
+export const SSE_URL = window.__ENV__.VITE_SSE_URL;
