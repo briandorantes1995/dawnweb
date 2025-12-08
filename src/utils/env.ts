508db8ca05c5b5f2deb9ea8148815a7f278af26c
Brian Dorantes <briandorantes@hotmail.com>
2025-12-08 13:56:52 -0600

    prueba de env 4

--- src/utils/env.ts
+++ src/utils/env.ts
@@ -1,3 +1,4 @@
 // utils/env.ts
-export const API_URL = window.__ENV__.VITE_API_URL;
-export const SSE_URL = window.__ENV__.VITE_SSE_URL;
+export const API_URL = window.__ENV__?.VITE_API_URL ?? import.meta.env.VITE_API_URL;
+export const SSE_URL = window.__ENV__?.VITE_SSE_URL ?? import.meta.env.VITE_SSE_URL;
+
